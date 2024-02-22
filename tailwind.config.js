20e536033f5f4ed2d100f1458580e380d9bed283
vaishnusd <vaishnu@ehs4safety.com>
2024-02-22 14:19:32 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [],
+};
+
