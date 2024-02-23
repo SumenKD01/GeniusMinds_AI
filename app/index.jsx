02130064bf29b444fdb0ca8c08f159ec4b93c735
Sumen <sumen@ehs4safety.com>
2024-02-23 11:55:09 +0530

    Made some changes in my branch

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Stack } from "expo-router";
 export default function Home() {
   return (
     <View   className='flex-1 bg-red-500 pt-10' >
-      <Text className='bg-fuchsia-800'>Hi hell hor nvjfnvjdfnkjvno</Text>
+      <Text className='bg-fuchsia-800'>Hi hell hor nvjfnvjdfnkjvno, Main Hu Jian, Main hu bada Takatwar!</Text>
       <StatusBar style='auto' backgroundColor='red'/>
     </View>
   );
