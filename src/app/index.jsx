3a32ee6e2399d24a60a085921f5e09b7ae2638da
Sumen <sumen@ehs4safety.com>
2024-02-27 17:30:53 +0530

    Made for the Vaishnu

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { View,Text, Pressable,StyleSheet } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import Login from '../LoginPage/Login';
@@ -10,17 +10,12 @@ import { store } from '../redux/store';
 export default function Root() {
   return (
     <Provider store={store}>
-
-    <SafeAreaView style={{backgroundColor:'blue', flex:1}}  >
-    
-      <View style={styles.container}>
-        <Login />
-      </View>
-
+      <SafeAreaView style={{ backgroundColor: 'blue', flex: 1 }}  >
+        <View style={styles.container}>
+          <Login />
+        </View>
       </SafeAreaView>
-      </Provider>
-
-
+    </Provider>
   )
 }
 
