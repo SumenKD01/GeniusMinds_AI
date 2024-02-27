5461188571ba0c84ba15f3b358f43670d734fd4d
vaishnusd <vaishnu@ehs4safety.com>
2024-02-27 17:06:57 +0530

    Merged with Sumen

--- src/app/(tabs)/index.jsx
+++ src/app/(tabs)/index.jsx
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import Homepage from '../../Homepage/Homepage';
 
 export default function Home() {
   return (
     <View className='flex-1 bg-slate-700'  >
-      <Text>Feed fvfvv screen</Text>
-      <StatusBar style='auto'/>
+      <Homepage/>
 
     </View>
   );
